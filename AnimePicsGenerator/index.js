f1471c7f2079571cb1630f06b1cd42025066f9e2
Digbijaya Lenka <digbijayalenka@gmail.com>
2023-04-09 02:08:41 +0530

    New App added Anime Pic
    Anime Pic Genrator

---
+++ AnimePicsGenerator/index.js
@@ -0,0 +1,26 @@
+const btnEl = document.getElementById("btn");
+const animeContainerEl = document.querySelector(".anime-container");
+const animeImgEl = document.querySelector(".anime-img");
+const animeNameEl = document.querySelector(".anime-name");
+
+btnEl.addEventListener("click", async function(){
+    try {
+        btnEl.disabled = true;
+        btnEl.innerText = "Loading...";
+        animeNameEl.innerText = "Updating...";
+        animeImgEl.src = "loader.svg";
+        const response = await fetch("https://api.catboys.com/img");
+        const  data = await response.json();
+        btnEl.disabled = false;
+        btnEl.innerText = "Get another Anime";
+        animeContainerEl.style.display = "block";
+        animeImgEl.src = data.url;
+        animeNameEl.innerText = data.artist;
+        
+    } catch (error) {
+        console.log("error");
+        btnEl.disabled = false;
+        btnEl.innerText = "Get Anime";
+        animeNameEl.innerText = "An error happened please try again";
+    }
+})
